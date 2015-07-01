e26329e39977a3738f20ecaa1de17570f2834dcc
ACER <ACER@Diego>
2015-07-01 06:55:31 -0500

    cambie una linea en el formulario clientes

--- AutoSoftV2/src/java/entidades/Clientes.java
+++ AutoSoftV2/src/java/entidades/Clientes.java
@@ -6,7 +6,7 @@ public class Clientes {
   * @author: Juan Diego Arango
   * @creationDate: 27 Octubre del 2014
   * @version: Autosoft 1.0
-  * 
+  * cambiandoesto
   */
     
     private String id_Cliente;

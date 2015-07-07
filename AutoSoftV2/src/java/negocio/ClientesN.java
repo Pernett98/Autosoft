2e1c67df69d688e882ff8336f0d20f654ebcfee6
mediateca <mediateca@mediatecnica5.poliacademico.edu.loc>
2015-07-07 10:25:15 -0500

    Ensayo pc

--- AutoSoftV2/src/java/negocio/ClientesN.java
+++ AutoSoftV2/src/java/negocio/ClientesN.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Ensayando esto
+
 /**
  *
  * @author Cristian
